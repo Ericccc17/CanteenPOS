fd4bd2cdac980a53dc3660c03c565357e1cfb4eb
Ericccc17 <115631845+Ericccc17@users.noreply.github.com>
2026-09-06 18:19:05 +0800

    Add caching for assets in service worker
    Implement service worker caching for assets.

--- sw.js
+++ sw.js
@@ -1,9 +1,22 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
+const CACHE_NAME = 'canteen-pos-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
